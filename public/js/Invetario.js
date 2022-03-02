894587dae7a7e867a2687d55289cadeb7e07bbad
Christian Emmanuel Yañez Gonzalez <laithg0279@gmail.com>
2022-03-02 14:14:40 -0700

    Invetario

---
+++ public/js/Invetario.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+    $('[name="modificar_1"]').click(() => {
+        $("#modal").modal();
+    });
+
+    $("#foto").change(() => {
+        let archivo = $("#foto")[0].files[0];
+        let reader = new FileReader();
+        let img = $("#foto_pro")[0];
+        let nombre = $("#nombreFoto")[0];
+        if (archivo) {
+            nombre.innerText = archivo.name;
+            reader.readAsDataURL(archivo);
+            reader.onloadend = () => img.src = reader.result;
+        }
+    });
+
+});
